e07b5efeb67656647ee5698937fa7827d5138a33
Geert Ijewski <ijewski.geertmartin@fh-swf.de>
2017-05-24 15:01:24 +0200

    simulation: loop executes once per second

--- code/simulation/main.c
+++ code/simulation/main.c
@@ -4,6 +4,56 @@
  * 
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <time.h>
+#include <sys/timerfd.h>
+#include <poll.h>
+#include <unistd.h>
+#include <math.h>
+
+/**
+ * \brief creates a timerfd
+ *
+ * Creates a timerfd, with the specified inital expiration and interval values.
+ * \param secs the second part of the inital expiration and interval value 
+ * \param secs the nanosecond partof the inital expiration and interval value
+ * \return a valid fd or something < 0
+ */
+int create_timerfd(int secs, int nsecs) {
+	int tfd;
+	if ((tfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK)) == 1) {
+        perror("timerfd_create");
+        return tfd;
+    }
+    struct itimerspec timer = {.it_interval.tv_sec = secs, .it_value.tv_sec = secs, 
+		.it_interval.tv_nsec = nsecs, .it_value.tv_nsec = nsecs};
+    if (timerfd_settime(tfd, 0, &timer, NULL) == -1) {
+        perror("timerfd_set");
+        return tfd;
+    }
+	
+	return tfd;
+}
+
+const char* get_time() {
+	static char buffer[32] = {0};
+
+	struct timespec spec;
+	clock_gettime(CLOCK_REALTIME, &spec);
+
+	time_t s = spec.tv_sec;
+	long ms = lround(spec.tv_nsec / 1.0e6); // todo: man math_error
+
+	int ret = snprintf(buffer, sizeof(buffer), "[%"PRIdMAX".%03ld]", (intmax_t)s, ms);
+	if(ret > sizeof(buffer) || ret < 0) {
+		perror("get_time(): snprintf");
+		exit(-1);
+	}
+	
+	return buffer;
+}
 
 int main(int argc, char** argv) {
 	/* arg parsing */
@@ -15,10 +65,33 @@ int main(int argc, char** argv) {
 	int numberOfRobots = atoi(argv[1]);
 	
 	/* init */
+#ifdef DEBUG
+	#define INTERVALL_S 0
+	#define INTERVALL_NS 100*1000*1000
+#else
+	#define INTERVALL_S 1
+	#define INTERVALL_NS 0
+#endif
+	int timer = create_timerfd(INTERVALL_S, INTERVALL_NS);
+	if(timer < 0) {
+		return 1;
+	}
 	
 	/* simulation loop */
-	while(1) {
+	struct pollfd pfd[] = {
+        {.fd = timer, .events = POLLIN, 0},
+	};
+	int s;
+	while((s = poll(pfd, (sizeof(pfd) / sizeof(pfd[0])), -1)) > 0) {
 		/* re-arm timer */
+		if((pfd[0].revents & POLLIN) == POLLIN) {
+			uint64_t expired;
+
+			pfd[0].revents = 0;
+			read(pfd[0].fd, &expired, sizeof(uint64_t));
+			
+			printf("%s next simulation step\n", get_time());
+		}
 		
 		/* get all robot commands */
 		
