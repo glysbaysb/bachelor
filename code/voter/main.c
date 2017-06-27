bf1050092250cff3d2f19ff0d7079739ff5151d1
Geert Ijewski <gm.ijewski@web.de>
2017-06-27 20:18:24 +0200

    voter: make all robots move

--- code/voter/main.c
+++ code/voter/main.c
@@ -4,9 +4,12 @@
 
 void worldStatusCallback(const WorldStatus* ws, void* additional);
 void worldStatusCallback(const WorldStatus* ws, void* additional) {
-	(void) additional;
 	for(size_t i = 0; i < ws->numOfObjects; i++) {
 		printf("object %d is a %s\n", ws->objects[i].id, ws->objects[i].type == ROBOT ? "robot" : "fuel station");
+
+		if(ws->objects[i].type == ROBOT) {
+			MoveRobot(additional, ws->objects[i].id, 1, 1);
+		}
 	}
 }
 
@@ -23,11 +26,10 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	startProcessingWorldEvents(worldCtx, &worldStatusCallback, NULL);
+	startProcessingWorldEvents(worldCtx, &worldStatusCallback, worldCtx);
 
 	while(1) {
 		sleep(1);
-		//MoveRobot(worldCtx, 0, -1, 1);
 	}
 
 	detachFromWorld(worldCtx);
