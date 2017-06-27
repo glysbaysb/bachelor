fc6975a07beafbe058b796adfe0395858a5ccca2
Geert Ijewski <gm.ijewski@web.de>
2017-06-27 20:51:48 +0200

    voter: use createRobot() and only move my own robot

--- code/voter/main.c
+++ code/voter/main.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 #include "world.h"
 
+static int myRobot = 0;
+
 void worldStatusCallback(const WorldStatus* ws, void* additional);
 void worldStatusCallback(const WorldStatus* ws, void* additional) {
 	for(size_t i = 0; i < ws->numOfObjects; i++) {
 		printf("object %d is a %s\n", ws->objects[i].id, ws->objects[i].type == ROBOT ? "robot" : "fuel station");
 
-		if(ws->objects[i].type == ROBOT) {
+		if(ws->objects[i].type == ROBOT && myRobot == ws->objects[i].id) {
 			MoveRobot(additional, ws->objects[i].id, 1, 1);
 		}
 	}
@@ -26,6 +28,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	myRobot = createRobot(worldCtx);
 	startProcessingWorldEvents(worldCtx, &worldStatusCallback, worldCtx);
 
 	while(1) {
