25c620470cec50fe06e3ae3a7533ba60cedfbddc
Geert Ijewski <gm.ijewski@web.de>
2017-07-13 14:55:59 +0200

    rpc: simplified parseRPCReply()
    The RPC format specifies that the first element will be an array with a fixed number of elements.
    There's no need to parse the buffer any further, anything after that isn't part of the RPC
    message anyway
    
    Luckily one call to msgpack_unpack_buffer() is enough to (recursively) unpack all elements of
    an array

--- code/voter/rpc.c
+++ code/voter/rpc.c
@@ -49,57 +49,65 @@ static int parseRPCReply(char* buf, size_t len, struct RPCReply* reply) {
 					"float", "str", "array", "map", "bin", "ext"};
 	size_t off = 0;
 	int ret = msgpack_unpack_next(&result, buf, len, &off);
-	while (ret == MSGPACK_UNPACK_SUCCESS) {
-		msgpack_object obj = result.data;
-
-		switch(obj.type) {
-		case MSGPACK_OBJECT_ARRAY:{
-			msgpack_object_array* arr = (msgpack_object_array*)&obj.via;
-			if(arr->size < 4) {
-				fprintf(stdout, "array too small");
-				break;
-			}
-
-			for(size_t i = 0; i < arr->size; i++) {
-				printf("arr elem %zu is a %s\n", i, typeToStr[arr->ptr[i].type]);
-			}
-			reply->op = arr->ptr[0].via.i64;
-			reply->id = arr->ptr[1].via.i64;
-			reply->error = arr->ptr[2].via.i64;
-
-			msgpack_object_array* paramArr = ((msgpack_object_array*)&arr->ptr[3].via);
-			size_t numOfParams = paramArr->size;
-			if(!(reply->params = calloc(numOfParams, sizeof(int)))) {
-				fprintf(stdout, "can't alloc for params");
-				break;
-			}
-			for(size_t i = 0; i < numOfParams; i++) {
-				reply->params[i] = paramArr->ptr[i].via.i64;
-			}
-
-			break;
-		}
-		default:
-			fprintf(stdout, "unhandled msgpack type:");
-			msgpack_object_print(stdout, obj);
-			putchar('\n');
-			break;
-		}
-
-		ret = msgpack_unpack_next(&result, buf, len, &off);
+	if (ret != MSGPACK_UNPACK_SUCCESS) {
+		msgpack_unpacked_destroy(&result);
+		return -1;
 	}
+	msgpack_object obj = result.data;
+
+	switch(obj.type) {
+	case MSGPACK_OBJECT_ARRAY:{
+		msgpack_object_array* arr = (msgpack_object_array*)&obj.via;
+		if(arr->size < 4) {
+			fprintf(stdout, "array too small");
+			break;
+		}
+
+		for(size_t i = 0; i < 3; i++) {
+			if(arr->ptr[i].type != MSGPACK_OBJECT_POSITIVE_INTEGER)
+				ret = -2;
+				break;
+		}
+		reply->op = arr->ptr[0].via.i64;
+		reply->id = arr->ptr[1].via.i64;
+		reply->error = arr->ptr[2].via.i64;
+
+		msgpack_object_array* paramArr = ((msgpack_object_array*)&arr->ptr[3].via);
+		size_t numOfParams = paramArr->size;
+		if(!(reply->params = calloc(numOfParams, sizeof(int)))) {
+			fprintf(stdout, "can't alloc for params");
+			break;
+		}
+		for(size_t i = 0; i < numOfParams; i++) {
+			reply->params[i] = paramArr->ptr[i].via.i64;
+		}
+
+		ret = 0;
+		break;
+	}
+	default:
+		fprintf(stdout, "unhandled msgpack type:");
+		msgpack_object_print(stdout, obj);
+		putchar('\n');
+		ret = -1;
+		break;
+	}
+
 	msgpack_unpacked_destroy(&result);
 
-	return ret != MSGPACK_UNPACK_PARSE_ERROR;
+	return ret;
 }
 
 int handleRPC(void* rpc_, char* buf, size_t len) {
 	RPCContext* rpc = (RPCContext*)rpc_;
 	struct RPCReply reply;
 
-	parseRPCReply(buf, len, &reply);
-
 	int r = -1;
+	if((r = parseRPCReply(buf, len, &reply)) < 0) {
+		fprintf(stderr, "can't parse rpc reply: %d\n", r);
+		return -1;
+	}
+
 	for(int i = 0; i < rpc->numRPCsInFlight; i++) {
 		if(rpc->rpcsInFlight[i].id != reply.id)
 			continue;
