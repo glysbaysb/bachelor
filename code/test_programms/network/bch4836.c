de863c3c5df0ee6e03dff40aa0828a7c978da838
Geert Ijewski <gm.ijewski@web.de>
2017-08-31 17:54:37 +0200

    bch4836: simple transformations

--- code/test_programms/network/bch4836.c
+++ code/test_programms/network/bch4836.c
@@ -50,69 +50,63 @@
 #include <math.h>
 #include <stdio.h>
 
-int             m = 6, n = 63, k = 36, t = 2, d = 5;
-int				length = 48;
-int             p[7];		/* irreducible polynomial */
+const int       m = 6, n = 63, k = 36, t = 2, d = 5;
+const int       length = 48;
+const int       p[7] = {1, 1, 0, 0, 0, 0, 1};		/* irreducible polynomial */
 int             alpha_to[64], index_of[64], g[13];
 int             recd[48], data[36], bb[13];
 int             numerr, errpos[64], decerror = 0;
-int             seed;
 
 
-void 
-read_p()
-/* Primitive polynomial of degree 6 */
-{
-	register int    i;
-    p[0] = p[1] = p[6] = 1; p[2] = p[3] = p[4] = p[5] =0;
-}
-
-
-void 
-generate_gf()
 /*
  * generate GF(2**m) from the irreducible polynomial p(X) in p[0]..p[m]
  * lookup tables:  index->polynomial form   alpha_to[] contains j=alpha**i;
  * polynomial form -> index form  index_of[j=alpha**i] = i alpha=2 is the
  * primitive element of GF(2**m) 
  */
+void generate_gf()
 {
-	register int    i, mask;
-	mask = 1;
+	int mask = 1;
+
 	alpha_to[m] = 0;
-	for (i = 0; i < m; i++) {
+
+	for (int i = 0; i < m; i++) {
 		alpha_to[i] = mask;
 		index_of[alpha_to[i]] = i;
 		if (p[i] != 0)
 			alpha_to[m] ^= mask;
 		mask <<= 1;
 	}
+
 	index_of[alpha_to[m]] = m;
 	mask >>= 1;
-	for (i = m + 1; i < n; i++) {
+
+	for (int i = m + 1; i < n; i++) {
 		if (alpha_to[i - 1] >= mask)
 		  alpha_to[i] = alpha_to[m] ^ ((alpha_to[i - 1] ^ mask) << 1);
 		else
 		  alpha_to[i] = alpha_to[i - 1] << 1;
 		index_of[alpha_to[i]] = i;
 	}
+
 	index_of[0] = -1;
 }
 
 
-void 
-gen_poly()
 /* 
  * Compute generator polynomial of BCH code of length = 48, redundancy = 12
  * (OK, this is not very efficient, but we only do it once, right? :)
  */
+void gen_poly()
 {
-	register int    ii, jj, ll, kaux;
+	int    ii, jj, ll, kaux;
 	int             test, aux, nocycles, root, noterms, rdncy;
 	int             cycle[13][7], size[13], min[13], zeros[13];
+
 	/* Generate cycle sets modulo 63 */
 	cycle[0][0] = 0; size[0] = 1;
 	cycle[1][0] = 1; size[1] = 1;
+
 	jj = 1;			/* cycle set index */
 	do {
 		/* Generate the jj-th cycle set */
@@ -123,49 +117,63 @@ gen_poly()
 			size[jj]++;
 			aux = (cycle[jj][ii] * 2) % n;
 		} while (aux != cycle[jj][0]);
+
 		/* Next cycle set representative */
 		ll = 0;
 		do {
 			ll++;
 			test = 0;
-			for (ii = 1; ((ii <= jj) && (!test)); ii++)	
-			/* Examine previous cycle sets */
-			  for (kaux = 0; ((kaux < size[ii]) && (!test)); kaux++)
-					if (ll == cycle[ii][kaux])
+			for (ii = 1; ((ii <= jj) && (!test)); ii++)	{
+				/* Examine previous cycle sets */
+				for (kaux = 0; ((kaux < size[ii]) && (!test)); kaux++) {
+					if (ll == cycle[ii][kaux]) {
 						test = 1;
+					}
+				}
+			}
 		} while ((test) && (ll < (n - 1)));
+
 		if (!(test)) {
 			jj++;	/* next cycle set index */
 			cycle[jj][0] = ll;
 			size[jj] = 1;
 		}
 	} while (ll < (n - 1));
+
 	nocycles = jj;		/* number of cycle sets modulo n */
 	/* Search for roots 1, 2, ..., d-1 in cycle sets */
 	kaux = 0;
 	rdncy = 0;
 	for (ii = 1; ii <= nocycles; ii++) {
 		min[kaux] = 0;
-		for (jj = 0; jj < size[ii]; jj++)
-			for (root = 1; root < d; root++)
-				if (root == cycle[ii][jj])
+		for (jj = 0; jj < size[ii]; jj++) {
+			for (root = 1; root < d; root++) {
+				if (root == cycle[ii][jj]) {
 					min[kaux] = ii;
+				}
+			}
+		}
+
 		if (min[kaux]) {
 			rdncy += size[min[kaux]];
 			kaux++;
 		}
 	}
+
 	noterms = kaux;
 	kaux = 1;
-	for (ii = 0; ii < noterms; ii++)
+	for (ii = 0; ii < noterms; ii++) {
 		for (jj = 0; jj < size[min[ii]]; jj++) {
 			zeros[kaux] = cycle[min[ii]][jj];
 			kaux++;
 		}
-	printf("This is a (%d, %d, %d) binary BCH code\n", length, k, d);
+	}
+
+
 	/* Compute generator polynomial */
 	g[0] = alpha_to[zeros[1]];
 	g[1] = 1;		/* g(x) = (X + zeros[1]) initially */
+
 	for (ii = 2; ii <= rdncy; ii++) {
 	  g[ii] = 1;
 	  for (jj = ii - 1; jj > 0; jj--)
@@ -175,6 +183,7 @@ gen_poly()
 	      g[jj] = g[jj - 1];
 	  g[0] = alpha_to[(index_of[g[0]] + zeros[ii]) % n];
 	}
+
 	printf("g(x) = ");
 	for (ii = 0; ii <= rdncy; ii++) {
 	  printf("%d", g[ii]);
@@ -185,128 +194,150 @@ gen_poly()
 }
 
 
-void 
-encode_bch()
 /* 
  * Calculate redundant bits bb[], codeword is c(X) = data(X)*X**(n-k)+ bb(X)
  */
+void encode_bch()
 {
-	register int    i, j;
-	register int    feedback;
-	for (i = 0; i < length - k; i++)
+	int    j;
+	int    feedback;
+
+	for (int i = 0; i < length - k; i++)
 		bb[i] = 0;
-	for (i = k - 1; i >= 0; i--) {
+
+	for (int i = k - 1; i >= 0; i--) {
 		feedback = data[i] ^ bb[length - k - 1];
+
 		if (feedback != 0) {
-			for (j = length - k - 1; j > 0; j--)
-				if (g[j] != 0)
+			for (j = length - k - 1; j > 0; j--) {
+				if (g[j] != 0) {
 					bb[j] = bb[j - 1] ^ feedback;
-				else
+				} else {
 					bb[j] = bb[j - 1];
+				}
+			}
 			bb[0] = g[0] && feedback;
 		} else {
-			for (j = length - k - 1; j > 0; j--)
+			for (j = length - k - 1; j > 0; j--) {
 				bb[j] = bb[j - 1];
+			}
 			bb[0] = 0;
-		};
-	};
-};
+		}
+	}
+}
 
 
-void 
-decode_bch()
 /*
  * We do not need the Berlekamp algorithm to decode.
  * We solve before hand two equations in two variables.
  */
+void decode_bch()
 {
-	register int    i, j, q;
+	int    i, j, q;
 	int             elp[3], s[5], s3;
 	int             count = 0, syn_error = 0;
 	int             loc[3], err[3], reg[3];
 	int				aux;
+
 	/* first form the syndromes */
 	printf("s[] = (");
 	for (i = 1; i <= 4; i++) {
 		s[i] = 0;
-		for (j = 0; j < length; j++)
-			if (recd[j] != 0)
+		for (j = 0; j < length; j++) {
+			if (recd[j] != 0) {
 				s[i] ^= alpha_to[(i * j) % n];
-		if (s[i] != 0)
+			}
+		if (s[i] != 0) {
 			syn_error = 1;	/* set flag if non-zero syndrome */
-							/* NOTE: If only error detection is needed,
-							 * then exit the program here...
-							 */
+		}
+
 		/* convert syndrome from polynomial form to index form  */
 		s[i] = index_of[s[i]];
 		printf("%3d ", s[i]);
 	};
 	printf(")\n");
+
 	if (syn_error) {	/* If there are errors, try to correct them */
 		if (s[1] != -1) {
 			s3 = (s[1] * 3) % n;
 			if ( s[3] == s3 )  /* Was it a single error ? */
-				{
+			{
 				printf("One error at %d\n", s[1]);
 				recd[s[1]] ^= 1;		/* Yes: Correct it */
+			}
+			/* Assume two errors occurred and solve
+			 * for the coefficients of sigma(x), the
+			 * error locator polynomail
+			 */
+			else {			
+				if (s[3] != -1) {
+					aux = alpha_to[s3] ^ alpha_to[s[3]];
+				} else {
+					aux = alpha_to[s3];
 				}
-			else {				/* Assume two errors occurred and solve
-								 * for the coefficients of sigma(x), the
-								 * error locator polynomail
-								 */
-                if (s[3] != -1)
-                  aux = alpha_to[s3] ^ alpha_to[s[3]];
-                else
-                  aux = alpha_to[s3];
 
 				elp[0] = 0;
 				elp[1] = (s[2] - index_of[aux] + n) % n;
 				elp[2] = (s[1] - index_of[aux] + n) % n;
+
 				printf("sigma(x) = ");
-				for (i = 0; i <= 2; i++)
+
+				for (i = 0; i <= 2; i++) {
 					printf("%3d ", elp[i]);
+				}
 				printf("\n");
 				printf("Roots: ");
+
 				/* find roots of the error location polynomial */
 				for (i = 1; i <= 2; i++)
 					reg[i] = elp[i];
 				count = 0;
-				for (i = 1; i <= 63; i++) { /* Chien search */
+
+				/* Chien search */
+				for (i = 1; i <= 63; i++) { 
 					q = 1;
-					for (j = 1; j <= 2; j++)
+					for (j = 1; j <= 2; j++) {
 						if (reg[j] != -1) {
 							reg[j] = (reg[j] + j) % n;
 							q ^= alpha_to[reg[j]];
 						}
-					if (!q) {	/* store error location number indices */
+					}
+
+					/* store error location number indices */
+					if (!q) {
 						loc[count] = i % n;
 						count++;
 						printf("%3d ", (i%n));
 					}
 				}
 				printf("\n");
-				if (count == 2)	
+
 				/* no. roots = degree of elp hence 2 errors */
-					for (i = 0; i < 2; i++)
+				if (count == 2)	{
+					for (i = 0; i < 2; i++) {
 						recd[loc[i]] ^= 1;
-				else	/* Cannot solve: Error detection */
+					}
+				}
+				/* Cannot solve: Error detection */
+				else {
 					printf("incomplete decoding\n");
 				}
 			}
+		}
 		else if (s[2] != -1) /* Error detection */
 			printf("incomplete decoding\n");
 	}
 }
 
 
-main()
+int main()
 {
-	int             i;
-	read_p();				/* read generator polynomial g(x) */
+	int             i, seed = 1;
 	generate_gf();			/* generate the Galois Field GF(2**m) */
 	gen_poly();				/* Compute the generator polynomial of BCH code */
 
-	seed = 1;
+	printf("This is a (%d, %d, %d) binary BCH code\n", length, k, d);
+
 	srandom(seed);
 	/* Randomly generate DATA */
 	for (i = 0; i < k; i++)
