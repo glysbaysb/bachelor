5c29af9245ed5b06ca777ff2513594541ac821c1
Geert Ijewski <gm.ijewski@web.de>
2017-09-20 21:39:10 +0200

    libecc: SingleByteErrorsTest

--- code/tests/ecc/test_ecc.cxx
+++ code/tests/ecc/test_ecc.cxx
@@ -5,7 +5,8 @@
  * The ECC is a RS (X, Y) code
  *
  * The tests are:
- * * 
+ * * all single byte-errors
+ * * a few multiple byte errors
  *
  * All test functions are member functions of ECCTest.
  * Most of the unit tests test "normally", i.e. they expect all funciton calls
@@ -17,22 +18,50 @@
 
 #include <libecc/ecc.h>
 
+#define MSG_LENGTH (255 - NPAR)
+#define RECVD_MSG_LENGTH (MSG_LENGTH + NPAR)
+
 class ECCTest: public ::testing::Test {
 private:
+	void generate_data(uint8_t* buff, size_t cnt) {
+		for(size_t i = 0; i < cnt; i++) {
+			buff[i] = i % 256;
+		}
+	}
+
 protected:
+	uint8_t message[MSG_LENGTH];
 public:
 	ECCTest() {
-		
+		initialize_ecc();
+		generate_data(message, sizeof(message));
 	}
 };
 
-TEST_F(ECCTest, TestName) {
+TEST_F(ECCTest, SingleByteErrorsTest) {
+	uint8_t  codeword[RECVD_MSG_LENGTH];
+	encode_data(message, MSG_LENGTH, codeword);
+
+	for(size_t i = 0; i < sizeof(codeword); i++) {
+		uint8_t copy[sizeof(codeword)];
+		memcpy(copy, codeword, sizeof(copy));
+
+		copy[i] ^= 0xAA;
+
+		decode_data(copy, RECVD_MSG_LENGTH);
+		if(check_syndrome () != 0) {
+			correct_errors_erasures (copy, 
+				 RECVD_MSG_LENGTH,
+				 0, 
+				 NULL);
+		}
+
+		ASSERT_TRUE(memcmp(copy, message, sizeof(message)) == 0)
+			<< "decoding error when corrupting position " << i;	
+	}
 	//EXPECT_EQ(addProcedure(rpc, (enum Procedure)1, &fake_callback, (void*)0xABCD9876), 0);
 
 //	FAIL() << "can't createRPCRequest()";
-	//	ASSERT_TRUE(sizeof(expected) <= outLen);
-//		ASSERT_TRUE(memcmp(expected, data, sizeof(expected)) == 0)
-//			<< "msgpacked data not as expected";	
 }
 
 int main(int argc, char** argv) {
