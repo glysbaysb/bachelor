52b980f3c53dd623798b983e5eeef6de8a92bfcc
Geert Ijewski <gm.ijewski@web.de>
2017-09-27 13:15:50 +0200

    implement an RPC echo test programm

---
+++ code/test_programms/network/full.cxx
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <iterator>
+#include <cstdint>
+#include <unistd.h>
+
+#include <msgpack.h>
+#include <libnetwork/network.h>
+#include <librpc/rpc.h>
+
+static void pack_msgpack(std::string str, msgpack_sbuffer sbuf);
+
+class Network {
+protected:
+	void* rpc;
+	ECCUDP udp;
+public:
+	Network() : udp("7777", "7777")
+	{
+		if((rpc = createRPCContext()) == nullptr) {
+			throw "can't create RPC context";
+		}
+	}
+
+	int addRPCHandler(Procedure num, TypeRPCProcedure handler, void* optional)
+	{
+		return addProcedure(rpc, num, handler, optional);
+	}
+
+	/* todo: find a better name */
+	void step()
+	{
+		auto packets = std::vector<Packet>();
+		if(udp.poll(0, packets) < 0) {
+			return;
+		}
+		
+		for(auto&& i : packets) {
+			handleRPC(rpc, i.data(), i.size());
+		}
+	}
+
+	int sendRPC(Procedure num, const void* paramsBuffer, size_t paramsLen)
+	{
+		uint8_t* buffer;
+		size_t bufferLen;
+		int r;
+		if((r = createRPCRequest(rpc, num, paramsBuffer, paramsLen, (void**)&buffer, &bufferLen)) < 0) {
+			return r;
+		}
+
+		auto p = Packet{buffer, buffer + bufferLen};
+		free(buffer);
+		return udp.send(p);
+	}
+
+	~Network()
+	{
+		destroyRPCContext(rpc);
+	}
+};
+
+void echo_callback(void* optional, msgpack_object_array* params)
+{
+    if(!params || !params->size) {
+        return;
+    }
+
+    for(size_t i = 0; i < params->size; i++) {
+        switch(params->ptr[i].type) {
+        case MSGPACK_OBJECT_ARRAY:
+            echo_callback(optional, (msgpack_object_array*) &params->ptr[i].via);
+            break;
+
+        case MSGPACK_OBJECT_FLOAT32:
+        case MSGPACK_OBJECT_FLOAT64:
+			std::cout << params->ptr[i].via.f64;
+            break;
+
+        case MSGPACK_OBJECT_NEGATIVE_INTEGER:
+			std::cout << params->ptr[i].via.u64;
+            break;
+
+        case MSGPACK_OBJECT_POSITIVE_INTEGER:
+			std::cout << params->ptr[i].via.i64;
+            break;
+
+        case MSGPACK_OBJECT_STR:
+			auto str = ((const msgpack_object_str*)&params->ptr[i].via.str);
+			std::copy(str->ptr, str->ptr + str->size, std::ostream_iterator<char>(std::cout));
+            break;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+	Network network;
+
+	network.addRPCHandler(Procedure::ECHO, echo_callback, nullptr);
+	while(1) {
+		/* stdin data? */
+		if(std::cin.rdbuf()->in_avail()) {
+			std::string line;
+			std::getline(std::cin, line);
+
+			msgpack_sbuffer sbuf;
+			msgpack_sbuffer_init(&sbuf);
+
+			pack_msgpack(line, sbuf);
+			network.sendRPC(Procedure::ECHO, sbuf.data, sbuf.size);
+			msgpack_sbuffer_destroy(&sbuf);
+		}
+
+		/* network data? */
+		network.step();
+
+		sleep(1);
+	}
+
+	return 0;
+}
+
+static void pack_msgpack(std::string str, msgpack_sbuffer sbuf)
+{
+    msgpack_packer pk;
+
+    /* serialize values into the buffer using msgpack_sbuffer_write callback function. */
+    msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
+
+    msgpack_pack_array(&pk, 1);
+    msgpack_pack_str(&pk, str.size());
+    msgpack_pack_str_body(&pk, str.c_str(), str.size());
+}
