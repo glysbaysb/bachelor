4a0c79a67eab520bccce4387cd230f9420397ef5
gmij <gm.ijewski@gmx.de>
2017-09-28 23:11:13 +0200

    add sample precommit checks & fix installation info & fix run-tests to exit early

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-build/tests/ecc/test_ecc
-build/tests/network/test_network
-build/tests/rpc/test_rpc
-build/tests/simulation/test_*
+build/tests/ecc/test_ecc || exit -1
+build/tests/network/test_network || exit -1
+build/tests/rpc/test_rpc || exit -1
+build/tests/simulation/test_* || exit -1
