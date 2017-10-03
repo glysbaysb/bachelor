2203949278706245cd36b41f9df6bacd43cf6052
gmij <gm.ijewski@gmx.de>
2017-10-03 18:37:05 +0200

    voter: more output

--- code/voter/main.c
+++ code/voter/main.c
@@ -9,10 +9,13 @@ void worldStatusCallback(const WorldStatus* ws, void* additional) {
 	printf("(%f:%f)\n", ws->xTilt, ws->yTilt);
 	for(size_t i = 0; i < ws->numOfObjects; i++) {
 		printf("object %d is a %s\n", ws->objects[i].id, ws->objects[i].type == ROBOT ? "robot" : "fuel station");
-		printf("(%f:%f\n",ws->objects[i].x, ws->objects[i].y);
+		printf("\t(%f:%f)\n",ws->objects[i].x, ws->objects[i].y);
 
 		if(ws->objects[i].type == ROBOT && myRobot == ws->objects[i].id) {
-			MoveRobot(additional, ws->objects[i].id, 1, 1);
+			int r = 0;
+			if((r = MoveRobot(additional, ws->objects[i].id, 1, 1)) < 0) {
+				fprintf(stderr, "can't move robot: %d", r);
+			}
 		}
 	}
 }
@@ -31,6 +34,7 @@ int main(int argc, char** argv) {
 	}
 
 	myRobot = createRobot(worldCtx);
+	printf("Created robot %d\n", myRobot);
 	startProcessingWorldEvents(worldCtx, &worldStatusCallback, worldCtx);
 
 	while(1) {
