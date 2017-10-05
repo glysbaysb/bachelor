e9e4549662111cfb3dfad398d5c8548e5c37604d
gmij <gm.ijewski@gmx.de>
2017-10-05 18:43:35 +0200

    test_algo: inital commit

---
+++ code/tests/algo/test_algo.cxx
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * @section DESCRIPTION
+ * The unit tests for the robot control algorithm reside in this file. 
+ *
+ * More specifically this file tests:
+ *
+ * All test functions are member functions of AlgoTest. Most of the the unit tests 
+ * test "normally",  * i.e. they expect all function calls to succed. Sometimes a unit
+ * test specifically  * tries to do something illegal to test the error path - to 
+ * improve clarity the comment explaining that block of code starts with "F:".
+ */
+#include <gtest/gtest.h>
+#include <memory>
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <experimental/optional>
+#include <libalgo/algo.h>
+
+class AlgoTest: public ::testing::Test { 
+public: 
+   AlgoTest( ) { 
+   } 
+
+   ~AlgoTest( )  { 
+   }
+
+};
+
+TEST_F(AlgoTest, Bla) {
+	ASSERT_EQ(0, 1);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS(); 
+}
+
