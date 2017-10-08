b8e7e5a8764002eeee07a3451745483f5ef00cc7
gmij <gm.ijewski@gmx.de>
2017-10-08 18:04:25 +0200

    voter: final version for the students

--- code/voter/main.c
+++ code/voter/main.c
@@ -2,44 +2,55 @@
 #include <unistd.h>
 #include <libworld/world.h>
 
-static int myRobot = 0;
+typedef struct {
+	int robot;
+	void* worldCtx;
+} Info;
 
 void worldStatusCallback(const WorldStatus* ws, void* additional);
-void worldStatusCallback(const WorldStatus* ws, void* additional) {
-	printf("(%f:%f)\n", ws->xTilt, ws->yTilt);
-	for(size_t i = 0; i < ws->numOfObjects; i++) {
-		printf("object %d is a %s\n", ws->objects[i].id, ws->objects[i].type == ROBOT ? "robot" : "fuel station");
-		printf("\t(%f:%f)\n",ws->objects[i].x, ws->objects[i].y);
+void worldStatusCallback(const WorldStatus* ws, void* additional)
+{
+	Info* info = (Info*)additional;
 
-		if(ws->objects[i].type == ROBOT && myRobot == ws->objects[i].id) {
+	printf("(%f:%f)\n", ws->xTilt, ws->yTilt);
+
+	for(size_t i = 0; i < ws->numOfObjects; i++) {
+		printf("#%d is a %s\n", ws->objects[i].id, ws->objects[i].type == ROBOT ? "robot" : "fuel station");
+		printf("\tPos: (%f:%f)\n", ws->objects[i].x, ws->objects[i].y);
+		printf("\tMass: %f\n", ws->objects[i].m);
+
+		if(ws->objects[i].type == ROBOT && info->robot == ws->objects[i].id) {
+			printf("\tFuel: %d\n", ws->objects[i].fuel);
+
 			int r = 0;
-			if((r = MoveRobot(additional, ws->objects[i].id, 1, 1)) < 0) {
+			if((r = MoveRobot(info->worldCtx, ws->objects[i].id, 1, 1)) < 0) {
 				fprintf(stderr, "can't move robot: %d", r);
 			}
 		}
 	}
 }
 
-int main(int argc, char** argv) {
-	void* worldCtx = NULL;
+int main(int argc, char** argv)
+{
+	Info callbackInfo = {0};
 
 	if(argc < 2) {
 		printf("%s host\n", argv[0]);
 		return 0;
 	}
 
-	if(!(worldCtx = connectToWorld(argv[1]))) {
+	if(!(callbackInfo.worldCtx = connectToWorld(argv[1]))) {
 		fprintf(stderr, "can't init world");
 		return 1;
 	}
 
-	myRobot = createRobot(worldCtx);
-	printf("Created robot %d\n", myRobot);
-	startProcessingWorldEvents(worldCtx, &worldStatusCallback, worldCtx);
+	callbackInfo.robot = createRobot(callbackInfo.worldCtx);
+	printf("Created robot %d\n", callbackInfo.robot);
+	startProcessingWorldEvents(callbackInfo.worldCtx, &worldStatusCallback, (void*)&callbackInfo);
 
 	while(1) {
 		sleep(1);
 	}
 
-	detachFromWorld(worldCtx);
+	detachFromWorld(callbackInfo.worldCtx);
 }
