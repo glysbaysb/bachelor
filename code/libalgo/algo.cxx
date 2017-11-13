f009c86141e292a46059d9ebbf5f90d0ba22c2fd
gmij <gm.ijewski@gmx.de>
2017-11-13 18:57:44 +0100

    libalgo: make algo recursive
    Precursor so in the next step, I can update the angle to the expected value in
    the next round. As the most critical robots move first, the robots not in a
    critical condition then counter those moves

--- code/libalgo/algo.cxx
+++ code/libalgo/algo.cxx
@@ -12,6 +12,53 @@ static bool _isInsideCircle(const Vector& position, const double& radius)
 	return squareDistance <= squareRadius;
 }
 
+static Action calc_movement(const std::pair<double, double>& angle, const std::vector<Robot>::reverse_iterator& robot,
+		const FuelStation& fuel)
+{
+	Vector accelleration(0., 0.);
+
+	/* if this robot might run out of fuel, move towards fuel station */
+	if(robot->crit() < CRIT_THRESHHOLD) {
+		auto dist = fuel.pos() - robot->pos();
+		accelleration = Vector(dist.x_ > 0 ? -1. : 1.,
+									dist.y_ > 0 ? -1. : 1.);
+	}
+	/* if it's */
+	else if(robot->crit() > CRIT_THRESHHOLD && robot->crit() < SAFE_THRESHHOLD) {
+	}
+	/* if it's completly safe, balance agressively */
+	else {
+		accelleration = Vector(angle.first > 0 ? -1. : 1.,
+									angle.second > 0 ? -1. : 1.);
+	}
+
+	/* would the robot fall? todo: current acceleration */
+	auto expectedPosition = robot->pos() + accelleration;
+
+	if(_isInsideCircle(expectedPosition, 50.)) {
+		return Action{robot->id(), accelleration};
+	} else {
+		return Action{robot->id(), {0., 0.}};
+	}
+}
+
+static std::vector<Action> calc_movement(const std::pair<double, double>& angle, const std::vector<Robot>& objects,
+		const FuelStation& fuel, const std::vector<Robot>::reverse_iterator& robot)
+{
+	if(robot == objects.rend()) {
+		return {};
+	}
+
+	auto actions = std::vector<Action>();
+	actions.push_back(calc_movement(angle, robot, fuel));
+
+	/* todo: update angle */
+	
+	auto otherActions = calc_movement(angle, objects, fuel, robot + 1);
+	actions.insert(std::end(actions), std::begin(otherActions), std::end(otherActions));
+	return actions;
+}
+ 
 std::vector<Action> calc_movement(const std::pair<double, double>& angle, std::vector<Robot>& objects,
 		const FuelStation& fuel)
 {
@@ -19,34 +66,7 @@ std::vector<Action> calc_movement(const std::pair<double, double>& angle, std::v
 	   $ fuel / distance $. In ascending order though ... */
 	std::sort(objects.begin(), objects.end());
 
-	/* ... so iterate backwards over the list -> move the robot with
+	/* ... so start the recursion with the last element -> move the robot with
 	   the most critical condition first */
-	auto actions = std::vector<Action>();
-	for(auto it = objects.rbegin(); it != objects.rend(); ++it) {
-		Vector accelleration(0., 0.);
-
-		/* if this robot might run out of fuel, move towards fuel station */
-		if(it->crit() < CRIT_THRESHHOLD) {
-			auto dist = fuel.pos() - it->pos();
-			accelleration = Vector(dist.x_ > 0 ? -1. : 1.,
-										dist.y_ > 0 ? -1. : 1.);
-		}
-		/* if it's */
-		else if(it->crit() > CRIT_THRESHHOLD && it->crit() < SAFE_THRESHHOLD) {
-		}
-		/* if it's completly safe, balance agressively */
-		else {
-			accelleration = Vector(angle.first > 0 ? -1. : 1.,
-										angle.second > 0 ? -1. : 1.);
-		}
-
-		/* would the robot fall? todo: current acceleration */
-		auto expectedPosition = it->pos() + accelleration;
-
-		if(_isInsideCircle(expectedPosition, 50.)) {
-			actions.push_back(Action{it->id(), accelleration});
-		}
-	}
-
-	return actions;
+	return calc_movement(angle, objects, fuel, objects.rbegin());
 }
