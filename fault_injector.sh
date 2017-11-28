c077753b61b5f08de0c65f4c77d740df2898f6fd
gmij <gm.ijewski@gmx.de>
2017-11-28 16:17:52 +0100

    thesis: einleitung

--- fault_injector.sh
+++ fault_injector.sh
@@ -3,27 +3,30 @@
 BIN=./build/controller/controller
 
 function start_limited() {
-	tput setaf 1
-	echo "limited"
-	tput op
+  ulimit -n 10 # max 10 open files
+  ulimit -v 1048576 # max 1MB virtual memory
 
-	ulimit -n 10 # max 10 open files
-	ulimit -v 1048576 # max 1MB virtual memory
-
-	$BIN "$@"
+  $BIN "$@"
 }
 
 while true; do
-	if (( RANDOM % 3)); then
-		$BIN "$@" &
-	else
-		start_limited "$@" &
-	fi
-	PID=$!
+  if (( RANDOM % 3)); then
+    tput setaf 1
+    echo "---------normal---------"
+    tput op
 
-	sleep $[ ($RANDOM % 8) + 2] # sleep between 2 and 10s
+    $BIN "$@" &
+  else
+    tput setaf 1
+    echo "---------limited---------"
+    tput op
 
-	kill -9 $PID
-	pkill -9 $BIN # kill all instances; can't get the PID, because then only
-				  # start_limited() would be killed, but not its children
+    start_limited "$@" &
+  fi
+  PID=$!
+
+  sleep $[ ($RANDOM % 8) + 2] # sleep between 2 and 10s
+
+  pkill -9 $BIN # kill all instances; can't use the PID, because then only
+                # start_limited() would be killed, but not its children
 done
