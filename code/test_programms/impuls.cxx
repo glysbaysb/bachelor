35979ae3a4fa9dd7abbfb29ff6f8243182e29582
gmij <gm.ijewski@gmx.de>
2018-03-14 18:45:14 +0100

    use realistic impuls values

--- code/test_programms/impuls.cxx
+++ code/test_programms/impuls.cxx
@@ -2,21 +2,17 @@
 #include <cmath>
 
 void test(float delta, float omega) {
-	const float S_0 = 0.1;
+	const float S_0 = 10;
 	for(auto t = 0; t < 10; t++) {
-		float s_t = S_0 * exp(-delta * t) *
-			((1 + omega * S_0) / omega * sin(omega * t) +
-			 S_0 + cos(omega * t));
+		float s_t = (S_0 * exp(-delta * t) * sin(omega * t));
 		std::cout << s_t << '\n';
 	}
 }
 
 int main(int argc, char **argv) {
-	test(1, 3);
-
 	for(auto i = 0; i < 10; i++) {
-		std::cout << "delta: " << i << '\n';
-		test(i, 1);
+		std::cout << "omega: " << i << '\n';
+		test(0.1, i);
 
 		std::cin.get();
 	}
